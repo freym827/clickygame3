10ff2a2f828c03fde4561b52fa9c1d7a73a5b906
Bitter Starglow <freym827@gmail.com>
2018-12-18 17:48:40 -0500

    Updates

---
+++ precache-manifest.18936d08023869ded88ac53434990fc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "949be40063e721b6195e70b019131112",
+    "url": "/clickygame3/static/media/nickback2.949be400.jpg"
+  },
+  {
+    "revision": "1266f5039341197711f4",
+    "url": "/clickygame3/static/js/runtime~main.1266f503.js"
+  },
+  {
+    "revision": "b7e0bc88f13408a0183e",
+    "url": "/clickygame3/static/js/main.b7e0bc88.chunk.js"
+  },
+  {
+    "revision": "5b265051865492349dc6",
+    "url": "/clickygame3/static/js/1.5b265051.chunk.js"
+  },
+  {
+    "revision": "b7e0bc88f13408a0183e",
+    "url": "/clickygame3/static/css/main.24aaccc3.chunk.css"
+  },
+  {
+    "revision": "536358b32a5be30d4a74ff55e6c7a815",
+    "url": "/clickygame3/index.html"
+  }
+];
