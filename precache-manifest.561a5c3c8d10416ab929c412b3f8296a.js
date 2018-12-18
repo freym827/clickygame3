5e97ccce706bb5308914e26f8efb4cd0863e129b
Bitter Starglow <freym827@gmail.com>
2018-12-18 17:39:33 -0500

    Updates

---
+++ precache-manifest.561a5c3c8d10416ab929c412b3f8296a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "949be40063e721b6195e70b019131112",
+    "url": "/clickygame/static/media/nickback2.949be400.jpg"
+  },
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "ce9534dedc3e59d8e328",
+    "url": "/clickygame/static/js/main.ce9534de.chunk.js"
+  },
+  {
+    "revision": "5b265051865492349dc6",
+    "url": "/clickygame/static/js/1.5b265051.chunk.js"
+  },
+  {
+    "revision": "ce9534dedc3e59d8e328",
+    "url": "/clickygame/static/css/main.7358fb78.chunk.css"
+  },
+  {
+    "revision": "9fb08863097dcd82d2d3b85f5a37da79",
+    "url": "/clickygame/index.html"
+  }
+];
--- src/components/NickPic/NickPic.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import './NickPic.css';
- const NickPic = props => (
-  <img onClick={() => props.shuffleNicks(props.id)} className="nickpics" alt={props.name} src={props.image} />
-);
- export default NickPic;
